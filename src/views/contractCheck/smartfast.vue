1dffe9758daff3c052beedd192b52426294ec6de
xayah <2117979238@qq.com>
2023-08-31 17:00:29 +0800

    update the transfer method

--- src/views/contractCheck/smartfast.vue
+++ src/views/contractCheck/smartfast.vue
@@ -19,6 +19,7 @@
                 <el-option v-for="version in solidityVersions" :key="version" :label="version" :value="version" />
               </el-select>
               <el-button @click="submitCode">提交</el-button>
+              <p>检测结果下载链接：<a :href="downloadLink" download>{{ downloadLinkText }}</a></p>
             </div>
           </el-card>
 
@@ -36,14 +37,28 @@
   import { ref } from 'vue';
   import CodeMirror from '@/components/CodeMirror/index.vue';
   import pieChart from '@/views/echarts/pieEcharts/index.vue';
+  import axios from 'axios';
   const selectedVersion = ref('');
   const codeValue = ref('');
   const solidityVersions = ['0.8.7', '0.7.6', '0.6.12']; // 示例Solidity版本列表
+  const downloadLink = ref(''); // 初始化为空
+  const downloadLinkText = ref('检测结果下载链接');
   
-  const submitCode = () => {
-    // 在这里处理提交代码的逻辑
-    console.log(codeValue.value);
-  };
+  const submitCode = async () => {
+  try {
+    const formData = new FormData();
+    formData.append('code', codeValue.value);
+    formData.append('solcVersion', selectedVersion.value);
+
+    const response = await axios.post('http://192.168.3.162:80/save', formData);
+    console.log('传输成功')
+    console.log(formData)
+    // 从后端返回的响应中获取下载链接
+    downloadLink.value = response.data.downloadLink;
+  } catch (error) {
+    console.error('Error submitting code:', error);
+  }
+};
   </script>
   
   <style lang="scss" scoped>
