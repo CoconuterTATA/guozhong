aab2f34a82738f10e6220153e7cdc8924987e081
CoconutTATA <2117979238@qq.com>
2023-11-07 16:02:29 +0800

    11.7

--- src/views/contractCheck/checkDectorRecords.vue
+++ src/views/contractCheck/checkDectorRecords.vue
@@ -4,6 +4,11 @@
         <el-card>
           <div>
             <span>Oyenet合约检测台</span>
+            <span class="version">solc版本</span>
+              <el-select v-model="selectedVersion">
+                <el-option v-for="version in solidityVersions" :key="version" :label="version" :value="version" />
+              </el-select>
+              <el-button @click="submitCode">提交</el-button>
           </div>
         </el-card>
       </div>
@@ -12,54 +17,60 @@
           <CodeMirror v-model="codeValue"/>
         </div>
         <div class="options-container">
-          <el-card>
-            <div>
-              <span>solc版本</span>
-              <el-select v-model="selectedVersion">
-                <el-option v-for="version in solidityVersions" :key="version" :label="version" :value="version" />
-              </el-select>
-              <el-button @click="submitCode">提交</el-button>
-              <div v-if="isLoading" id="Loading">
-      <!-- 加载动画 -->
-      <div class="loader-inner ball-beat">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
-              <!-- <p>检测报告下载链接：<a :href="downloadLink" download>{{ downloadLinkText }}</a></p> -->
-            </div>
-          </el-card>
-          <el-card>
 
-                <!-- <div ref="chartsRef" class="app-echarts"></div> -->
-<div class="card-content">
-  <img :src="imageUrl" alt="iconOfFWC" id="cardImage" class="iconFWC">
-        <h3>漏洞统计</h3>
-        <p>调用栈: {{ callstack }}</p>
-        <p>时间依赖: {{ time_dependency }}</p>
-        <p>重入: {{ reentrancy }}</p>
-        <p>整数溢出: {{ integer_overflow }}</p>
-        <p>多签名合约漏洞: {{ parity_multisig_bug_2 }}</p>
-        <p>整数下溢: {{ integer_underflow }}</p>
-        <p>货币并发: {{ money_concurrency }}</p>
-        <p>断言失败: {{ assertion_failure }}</p>
-        <h3>EVM代码覆盖率: {{ evm_code_coverage }}</h3>
-        <h3>合约类型: {{ contract_type }}</h3>
-</div>
-
-            <div class="result-show-text">
-              <el-card v-if="auditMessage">
-                <div v-html="auditMessage"></div>
-              </el-card>
-            </div>
-          </el-card>
         </div>
       </div>
 
       <div class="footer">
       </div>
     </div>
+    <!-- 模态框 -->
+    <div id="resultModal" class="modal" v-show="isModalVisible">
+  <!-- 模态框内容 -->
+  <div class="modal-content">
+    <span class="closeButton" @click="closeModal">&times;</span>
+    <div class="tabs" style="text-align:center; margin-top: 10px;"> <!-- 标签容器，居中显示 -->
+      <span 
+        class="tab" 
+        :class="{ selected: showResults }" 
+        @click="showResults = true"
+      >
+        Result
+      </span>
+      <span 
+        class="tab" 
+        :class="{ selected: !showResults }" 
+        @click="showResults = false"
+      >
+        Code
+      </span>
+    </div>
+    <div class="card-content">
+      <!-- 切换内容 -->
+      <div v-if="showResults">
+        <img :src="imageUrl" alt="iconOfFWC" id="cardImage" class="iconFWC">
+      <h3>漏洞统计</h3>
+      <p>调用栈: <span>{{ callstack.length > 0 ? callstack.join(', ') : '无' }}</span></p>
+      <p>时间依赖: <span>{{ time_dependency.length > 0 ? time_dependency.join(', ') : '无' }}</span></p>
+      <p>重入: <span>{{ reentrancy.length > 0 ? reentrancy.join(', ') : '无' }}</span></p>
+      <p>整数溢出: <span>{{ integer_overflow.length > 0 ? integer_overflow.join(', ') : '无' }}</span></p>
+      <p>多签名合约漏洞: <span>{{ parity_multisig_bug_2.length > 0 ? parity_multisig_bug_2.join(', ') : '无' }}</span></p>
+      <p>整数下溢: <span>{{ integer_underflow.length > 0 ? integer_underflow.join(', ') : '无' }}</span></p>
+      <p>货币并发: <span>{{ money_concurrency.length > 0 ? money_concurrency.join(', ') : '无' }}</span></p>
+      <p>断言失败: <span>{{ assertion_failure.length > 0 ? assertion_failure.join(', ') : '无' }}</span></p>
+      <h3>EVM代码覆盖率: <span>{{ evm_code_coverage ? evm_code_coverage + '%' : '无' }}</span></h3>
+      <h3>合约类型: <span>{{ contract_type || '无' }}</span></h3>
+      </div>
+      <div v-else>
+        <h3>提交的合约代码</h3>
+        <div class="editor-box"> <!-- 编辑框，可滚动 -->
+          <pre>{{ codeValue }}</pre>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
   </template>
 
   <script lang="ts" setup>
@@ -74,8 +85,7 @@
   const selectedVersion = ref('');
   const codeValue = ref('');
   const solidityVersions = ['0.8.0', '0.7.5', '0.7.4','0.7.3', '0.7.2', '0.7.1', '0.7.0', '0.5.0', '0.4.24']; // 示例Solidity版本列表
-  const downloadLink = "http://42.194.184.32:8080/download"; // 初始化为空
-  const downloadLinkText = ref('检测结果下载链接');
+  const showResults = ref(true); // 默认显示结果
 
   // const imageUrl = require('@/assets/image/FWCicon.png');
   const callstack = ref([])
@@ -88,70 +98,39 @@
   const assertion_failure = ref([])
   const evm_code_coverage = ref(0)
   const contract_type = ref("")
-
-
-  const chartsRef = ref<HTMLElement | null>()
-  const reminderValue = ref(0);
-  const optimizationValue = ref(0);
-  const highRiskValue = ref(0);
-  const lowRiskValue = ref(0);
-  const mediumRiskValue = ref(0);
-  const backgroundColor = '#101736';
-  const auditMessage = ref('');
+  const isModalVisible = ref(false);
   const isLoading = ref(false);
 
 
 onMounted(() => {
-  // chartInstance.value = echarts.init(chartsRef.value);
-  // chartInstance.value.setOption(options);
-  // let {addObserver} = useResizeElement(chartInstance.value, chartsRef.value);
-  // addObserver();
 })
-
+const closeModal = () => {
+  isModalVisible.value = false;
+};
   const submitCode = async () => {
+  isModalVisible.value = true;
+  console.log(isModalVisible.value)
   try {
     isLoading.value = true; 
     const formData = new FormData();
     formData.append('code', codeValue.value);
     formData.append('solcVersion', selectedVersion.value);
 
-    const response = await axios.post('/api/smartFast', formData);
+    const response = await axios.post('/api/oyente', formData);
 
     if (response.data) {
-        console.log('high:', response.data.high);
-        console.log('opt:', response.data.opt);
-        console.log('low:', response.data.low);
-        console.log('medium:', response.data.medium);
-        console.log('need attention:', response.data["need attention"]);
-
-        reminderValue.value = response.data["need attention"];
-        console.log(reminderValue.value)
-        optimizationValue.value = response.data.opt;
-        highRiskValue.value = response.data.high;
-        lowRiskValue.value = response.data.low;
-        mediumRiskValue.value = response.data.medium;
-        auditMessage.value = `
-            <div class="result-section">
-                <h1>审计结果</h1>
-                <p>审计合约名: <strong>${response.data.ContractName}</strong></p>
-                <p>审计合约版本: <strong>${response.data.ContractVersion}</strong></p>
-                <p>审计报告文件名: <strong>${response.data.ContractReport}</strong></p>
-                <p>经smartfast智能合约审计工具发现，该智能合约安全存在
-                <span style="color: #ff0000; font-weight: bold; font-size: 20px;">${response.data.high}个高风险漏洞</span>，
-                <span style="color: #f39c12; font-weight: bold; font-size: 20px;">${response.data.medium}个中风险漏洞</span>，
-                <span style="color: #27ae60; font-weight: bold; font-size: 20px;">${response.data.low}个低风险漏洞</span>，
-                <span style="color: #9b59b6; font-weight: bold; font-size: 20px;">${response.data["need attention"]}个地方需要注意</span>以及
-                <span style="color: #3498db; font-weight: bold; font-size: 20px;">${response.data.opt}个地方需要优化</span>，
-                详细合约审计结果可通过见检查结果下载链接。</p>
-                <p style="margin-top: 20px; display: flex; justify-content: space-between;">
-                        <span>检测耗时: ${response.data.ExecutedTime}</span>
-                        <span style="color: #7F7F7F">审计时间: ${response.data.AuditTime}</span>
-                </p>
-            </div>
-          `;
-
+      callstack.value = response.data.vulnerabilities.callstack;
+      time_dependency.value = response.data.vulnerabilities.time_dependency;
+      reentrancy.value = response.data.vulnerabilities.reentrancy;
+      integer_overflow.value = response.data.vulnerabilities.integer_overflow;
+      parity_multisig_bug_2.value = response.data.vulnerabilities.parity_multisig_bug_2;
+      integer_underflow.value = response.data.vulnerabilities.integer_underflow;
+      money_concurrency.value = response.data.vulnerabilities.money_concurrency;
+      assertion_failure.value = response.data.vulnerabilities.assertion_failure;
+      evm_code_coverage.value = parseFloat(response.data.evm_code_coverage);
+      contract_type.value = response.data.contract_type;
+      console.log(response.data)
     }
-
     console.log('传输成功')
     console.log(formData)
     // 从后端返回的响应中获取下载链接
@@ -164,6 +143,48 @@ onMounted(() => {
   </script>
 
   <style lang="scss" scoped>
+
+  .selected {
+  color: green;
+  border-bottom: 2px solid green;
+}
+
+/* 可以添加更多样式来美化文本 */
+.tab {
+  cursor: pointer;
+  padding: 5px;
+  margin: 0 10px;
+  display: inline-block; /* 让元素按照inline-block排列 */
+  font-size: 18px; /* 调整字体大小 */
+  transition: all 0.3s ease; /* 使状态变化有动画效果 */
+}
+.toggle-button {
+  /* 使按钮自身对齐到中心 */
+  align-self: center;
+  /* 可选：为按钮添加一些间距 */
+  // margin-top: 10px;
+  // margin-bottom: 10px;
+}
+.editor-box {
+  /* 设置编辑框的高度 */
+  height: 500px; /* 或者你希望的任何高度 */
+  /* 添加滚动条 */
+  overflow-y: auto;
+  /* 可选：添加内边距 */
+  padding: 15px;
+  /* 设置背景和字体颜色 */
+  background-color: white;
+  color: black;
+  /* 设置字体大小 */
+  font-size: 1em;
+  /* 设置滚动条样式 */
+  scrollbar-width: thin;
+  scrollbar-color: blue grey;
+  /* 增加边框，更好的区分编辑框 */
+  border: 1px solid #ccc;
+  /* 设置字体家族 */
+  font-family: 'Courier New', Courier, monospace;
+}
   
   .chart-wrapper {
   position: relative;
@@ -240,7 +261,9 @@ onMounted(() => {
   }
 }
 
-
+.version{
+  margin-left: 70%;
+}
   .result-show-text {
   position: relative;
   width: 100%;
@@ -249,12 +272,6 @@ onMounted(() => {
   transform: translateX(-50%);
   margin-top: -200px;
 }
-  // .card-content{
-  //   position: sticky;
-  // top: 10px;  /* 距离视口顶部的距离，可以根据需要调整 */
-  // width: 100%;
-  // height: 800px;  /* 这个高度可以根据你的饼图大小调整 */
-  // }
 
   .card-content {
     font-family: 'Roboto', sans-serif; /* 使用Roboto字体 */
@@ -263,12 +280,12 @@ onMounted(() => {
     top: 20px;
     width: 100%;
     // max-width: 600px; /* 限制最大宽度，增加可读性 */
-    height: 800px; /* 设置为auto使其根据内容调整 */
+    height: 40%; /* 设置为auto使其根据内容调整 */
     // padding: 20px; /* 增加内边距 */
     background-color: #ffffff; /* 背景颜色 */
     border-radius: 8px; /* 圆角 */
     // box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1); /* 增加一些阴影增加深度感 */
-    padding: 50px 20px 20px 10px;  /* 上, 右, 下, 左 */
+    padding: 50px 20px 60px 0px;  /* 上, 右, 下, 左 */
 
     /* 使用Flexbox使内容垂直居中 */
     // display: flex;
@@ -281,7 +298,7 @@ onMounted(() => {
     
     border-bottom: 1px solid #e0e0e0;  /* 添加底部边框 */
     margin-bottom: 15px;  /* 控制横线之间的间隔 */
-    padding-bottom: 10px;  /* 控制文字到横线的距离 */
+    padding-bottom: 15px;  /* 控制文字到横线的距离 */
 }
 
 .card-content p:last-of-type, .card-content h3:last-of-type {
@@ -289,9 +306,9 @@ onMounted(() => {
 }
 
 .iconFWC{
-  padding: 0px 0px 0px 450px;  /* 上, 右, 下, 左 */
-  width: 120px;
-  height: 120px;
+  padding: 0px 10px 10px 550px;  /* 上, 右, 下, 左 */
+  width: 190px;
+  height: 190px;
   top: 100px;
 }
 .app-echarts {
@@ -315,7 +332,9 @@ onMounted(() => {
       flex-grow: 1;
     }
 
-    & .editor-container,
+    & .editor-container{
+      width: 100%;
+    }
     & .options-container {
       flex: 1;
       padding: 10px;
@@ -326,4 +345,60 @@ onMounted(() => {
       flex-shrink: 0;
     }
   }
+
+  /* 模态框（背景） */
+.modal {
+  // display: none; /* 默认隐藏 */
+  position: absolute;
+  z-index: 1; /* 位于顶层 */
+  left: 0;
+  top: 0;
+
+  width: 100%; /* 全屏宽 */
+  height: 100%; /* 全屏高 */
+  overflow: auto; /* 启用滚动条 */
+  background-color: rgb(0,0,0); /* 背景色 */
+  background-color: rgba(0,0,0,0.4); /* 背景色，带透明度 */
+}
+
+/* 模态框内容 */
+.modal-content {
+  // display: flex;
+  // align-items: center;
+  /* 方向为垂直 */
+  flex-direction: column;
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% 从顶部和居中定位 */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 50%; /* 宽度 */
+  height: 800px;
+  border-radius: 10px;
+}
+
+/* 关闭按钮 */
+.closeButton {
+  color: #241e1e;
+  // float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+/* 其他样式 */
+.iconFWC {
+  width: 50px; /* 图标宽度 */
+  height: auto; /* 高度自适应 */
+}
+
+.card-content {
+  text-align: left; /* 左对齐文本 */
+}
+
   </style>
